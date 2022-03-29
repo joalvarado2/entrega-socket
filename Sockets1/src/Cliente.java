395fc4fc2d42c929ffc5138fc7d72f9fabdd2e81
joalvarado2 <jonathanalva92@gmail.com>
2022-03-28 22:06:12 -0500

    errores de compilado

---
+++ Sockets1/src/Cliente.java
@@ -0,0 +1,132 @@
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.*;
+
+class Client {
+
+    static Scanner strInput = null;
+    private static int port = 9876;
+
+    public static void main(String[] args) throws ClassNotFoundException, IOException, InterruptedException {
+        strInput = new Scanner(System.in);
+        String choice, cont = "s";
+
+// limpio pantalla
+        Client.CleanScreen();
+
+        while (cont.equalsIgnoreCase("s")) {
+            System.out.println("\t\t *** Sistema de Informacion Bancario *** \n\n");
+
+            System.out.println("1 ===> Crear Nueva Cuenta ");
+            System.out.println("2 ===> Buscar Una Cuenta Especifica ");
+
+            System.out.print("\n\n");
+            System.out.println("Ingresa la opciÃ³n: ");
+            choice = strInput.nextLine();
+
+            if (choice.equals("1")) {
+                Client.AddRecord();
+            } else if (choice.equals("2")) {
+                Client.SearchRecordbyID();
+            }
+
+            System.out.println("Â¿Quieres continuar? S/N");
+            cont = strInput.nextLine();
+
+// limpio pantalla
+            Client.CleanScreen();
+        }
+
+// libero recursos
+        strInput.close();
+    }
+
+    private static void CleanScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    private static void AddRecord() throws IOException, ClassNotFoundException, InterruptedException {
+        InetAddress host = InetAddress.getLocalHost();
+        Socket socket = null;
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+
+        strInput = new Scanner(System.in);
+        String account, value, newRow;
+
+// limpio pantalla
+        Client.CleanScreen();
+
+        System.out.println("\t\t [ Crear Cuenta Bancaria ]\n");
+
+        System.out.print("Ingresa el NÃºmero de Cuenta: ");
+        account = strInput.nextLine();
+        System.out.print("Ingresa el Valor: ");
+        value = strInput.nextLine();
+
+        newRow = "1," + account + "," + value;
+
+// establecer conexiÃ³n de socket al servidor
+        socket = new Socket(host.getHostName(), port);
+
+// escribo en el socket usando ObjectOutputStream
+        oos = new ObjectOutputStream(socket.getOutputStream());
+        oos.writeObject(newRow);
+
+// leo el mensaje de respuesta del servidor
+        ois = new ObjectInputStream(socket.getInputStream());
+        String message = (String) ois.readObject();
+        System.out.print("\n");
+        System.out.println("Respuesta del Servidor: '" + message + "'");
+        System.out.print("\n");
+
+// libero recursos
+        ois.close();
+        oos.close();
+        Thread.sleep(100);
+    }
+
+    private static void SearchRecordbyID() throws IOException, ClassNotFoundException {
+        InetAddress host = InetAddress.getLocalHost();
+        Socket socket = null;
+        ObjectOutputStream oos = null;
+        ObjectInputStream ois = null;
+
+        String ID;
+        strInput = new Scanner(System.in);
+
+// limpio pantalla
+        Client.CleanScreen();
+
+        System.out.println("\t\t [ Buscar Cuenta ]\n");
+
+        System.out.println("Ingresa el NÃºmero de Cuenta: ");
+        ID = strInput.nextLine();
+
+// establecer conexiÃ³n de socket al servidor
+        socket = new Socket(host.getHostName(), port);
+
+// escribo en el socket usando ObjectOutputStream
+        oos = new ObjectOutputStream(socket.getOutputStream());
+
+        oos.writeObject("2," + ID);
+
+// leo el mensaje de respuesta del servidor
+        ois = new ObjectInputStream(socket.getInputStream());
+        String message = (String) ois.readObject();
+
+// imprimo la respuesta del servidor
+        System.out.print("\n");
+        System.out.println(" ------------------------------- ");
+        System.out.println("|       Cuenta        Valor      ");
+        System.out.println(" ------------------------------- ");
+
+        System.out.print(message);
+
+        System.out.println("|                                 |");
+        System.out.println(" ------------------------------- ");
+        System.out.print("\n");
+    }
+}
---
+++ Sockets1/src/Server.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.lang.ClassNotFoundException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.*;
+
+public class Server {
+
+    private static ServerSocket server;
+    private static int port = 9876;
+
+    public static void main(String args[]) throws IOException, ClassNotFoundException {
+        // crea el objeto del servidor de socket
+        server = new ServerSocket(port);
+        String responseMsj = null;
+        // sigue escuchando indefinidamente hasta que recibe una llamada de 'exit'
+        while (true) {
+            // creando socket y esperando la conexiÃ³n del cliente
+            System.out.println("Esperando Solicitud del cliente...");
+            Socket socket = server.accept();
+            // leer desde el socket al objeto ObjectInputStream
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            // convierte el objeto ObjectInputStream en String
+            String message = (String) ois.readObject();
+//---------------------------------------------------------------
+            switch (message.split(",")[0]) {
+                case "1":
+                    System.out.println("===> Agregando una nueva Cuenta");
+                    responseMsj = Server.AddRecord(message.substring(2));
+
+                    break;
+                case "2":
+                    System.out.println("===> Consultando Cuenta");
+                    responseMsj = Server.SearchRecordbyID(message.substring(2));
+                    break;
+                default:
+                    System.out.println("La opcion no es valida");
+            }
+            //---------------------------------------------------
+            // crea el objeto ObjectOutputStream
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+
+            // escribir objeto en Socket
+            oos.writeObject(responseMsj);
+
+            // libero recursos
+            ois.close();
+            oos.close();
+            socket.close();
+
+            // finalizar el servidor si el cliente envÃ­a una solicitud de salida
+            if (message.equalsIgnoreCase("exit")) {
+                break;
+            }
+        }
+        System.out.println("Apagando el servidor de Socket!!");
+        // cierra el objeto ServerSocket
+        server.close();
+    }
+
+    private static String AddRecord(String record) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter("datos.txt", true));
+        try {
+            // escribo los datos en el archivo
+            bw.write(record);
+            bw.flush();
+            bw.newLine();
+            // libero recursos
+            bw.close();
+        } catch (Exception e) {
+            return "NO-OK";
+        }
+        return "Registro grabado OK";
+    }
+
+    private static String SearchRecordbyID(String ID) throws IOException {
+        String record, account = "";
+        BufferedReader br = new BufferedReader(new FileReader("datos.txt"));
+        while ((record = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(record, ",");
+            if (record.contains(ID)) {
+                account += " " + st.nextToken() + " " + st.nextToken() + " \n";
+            }
+        }
+        // libero recursos
+        br.close();
+        return account;
+    }
+}
